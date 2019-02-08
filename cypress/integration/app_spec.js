89e08ed0c5f75f742ca8fdb04146d1cb174a2e85
Charles Kuo <CharlesK@ashn.com>
2019-02-07 18:19:48 -0800

    Initial Cypress test project setup

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -85,7 +85,7 @@ describe('TodoMVC - React', function () {
     // https://on.cypress.io/should
     // https://on.cypress.io/as
 
-    it('should allow me to add todo items', function () {
+    it.only('should allow me to add todo items', function () {
       // create 1st todo
       cy.get('.new-todo').type(TODO_ITEM_ONE).type('{enter}')
 
